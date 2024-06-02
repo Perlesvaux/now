96fced6f7453ad2e0626b3bed9b4d392ee092fdd
chico <99514791+Perlesvaux@users.noreply.github.com>
2024-06-02 07:29:18 -0600

    fully functional and good looking enough

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,22 @@ function App() {
     return sum;
   }
 
+  function setTimeStamp(e){
+    try {
+      e.preventDefault()
 
+      const sum = seconds()
+      const url_part = state.url.match(/https:\/\/(?:www\.youtube\.com|youtu\.be)\/(?:watch\?v=|live\/|)([a-zA-Z0-9_-]{11})/) 
 
-  function setTimeStamp(){
-    const sum = seconds()
-    const url_part = state.url.match(/https:\/\/(?:www\.youtube\.com|youtu\.be)\/(?:watch\?v=|live\/|)([a-zA-Z0-9_-]{11})/) 
+      const result = `https://youtu.be/${url_part[1]}?t=${sum}`
 
-    const result = `https://youtu.be/${url_part[1]}?t=${sum}`
+      navigator.clipboard.writeText(result);
 
-    setState({...state, out:result})
+      setState({...state, out:result})
+      
+    } catch (error) {
+      console.error(error)
+    }
 
   }
 
@@ -39,13 +46,42 @@ function App() {
 
   return (
     <>
-      <input type="url" name="url" onChange={userInput} value={state.url} className='form-control'/>
-      <input type="text" name="now" onChange={userInput} value={state.now} className='form-control text-center' placeholder='SS | MM:SS | HH:MM:SS'/>
-      <button onClick={setTimeStamp} className='btn btn-danger'>Ok!</button>
-      {state.out && <>{state.out}</>}
+      <div className='card text-center bg-danger'>
+        <img useMap="#pezote" width="307px" height="307px" src="./pezotenow.jpeg" className="coati card-img-bottom text-center" alt="Pezote buddy will help you out!" />
+      </div>
+
+
+      <div className='card bg-light'>
+          {state.out ?
+          <div className='card-header text-muted'>Copied to clipboard!</div> :
+          <div className='card-header text-muted'>Timestamp YT videos from your mobile!</div> }
+
+        <div className='card-body'>
+          <form onSubmit={setTimeStamp}>
+
+            <input type="url" name="url" onChange={userInput} value={state.url} className='form-control text-center' placeholder='URL' required/>
+            <input type="text" name="now" onChange={userInput} value={state.now} className='form-control text-center' placeholder='SS | MM:SS | HH:MM:SS' required/>
+            <button type="submit" style={{display:"none"}}> ok </button>
+
+            <map name="pezote">
+              <area onClick={(e)=>{e.target.parentElement.parentElement.requestSubmit()}} shape="rect" coords="138,164,298, 279" alt="Nosy pezote!" className="pointer"/>
+            </map>
+
+          </form>
+        </div>
+        
+          {state.out && <div className='card-footer text-muted'>{state.out}</div>}
+        </div>
+      
 
     </>
   )
 }
+
+
+
+            // <div className="d-grid gap-2">
+            //   <button className='btn btn-danger'>Ok!</button>
+            // </div>
 
 export default App
